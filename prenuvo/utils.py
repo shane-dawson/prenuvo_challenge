bc5175871a0f7f31ee2090fe2b0520114784c1ea
Shane <signalterror+gitlab@gmail.com>
2018-12-19 19:12:58 -0800

    Calculate gif dimensions from its header

--- prenuvo/utils.py
+++ prenuvo/utils.py
@@ -2,8 +2,8 @@ from urllib.request import urlopen
 
 from PIL import ImageFile
 
-
 def get_gif_dimensions(uri):
+    """ Read the file in chunks until pillow has enough header info to extract the dimensions"""
     with urlopen(uri) as file:
         parser = ImageFile.Parser()
         while True:
