678b909262ef79107e287a128fecff703cfce5be
Shane <signalterror+gitlab@gmail.com>
2018-12-20 08:20:19 -0800

    Add gif to webp conversion method

--- prenuvo/utils.py
+++ prenuvo/utils.py
@@ -1,6 +1,8 @@
+import subprocess
 from urllib.request import urlopen
 
 from PIL import ImageFile
+
 
 def get_gif_dimensions(uri):
     """ Read the file in chunks until pillow has enough header info to extract the dimensions"""
@@ -14,3 +16,9 @@ def get_gif_dimensions(uri):
             if parser.image:
                 return parser.image.size
         return [1, 1]
+
+
+def convert_gif_webp(path):
+    cmd = f'ffmpeg -i {path}/bw-gif.gif -movflags faststart -loop 0 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {path}/bw-gif.webp'
+    p = subprocess.Popen(cmd, shell=True)
+    p.communicate()
